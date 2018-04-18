0154ba6c6b56218e6fb06b7d672462b6a12990aa
unibtc <unibtc@gmail.com>
2018-04-18 18:41:28 +0800

    parity-js/api fork

---
+++ README.md
@@ -0,0 +1,3 @@
+# bitclimb-parity
+
+Heavily based on [parity/api](https://github.com/parity-js/api) with some minor changes to meet the needs of bitclimb.
---
+++ src/library.js
@@ -0,0 +1,5 @@
+global.fetch = require('node-fetch');
+const Abi = require('@parity/abi');
+const Api = require('./index');
+
+module.exports = { Api, Abi };
